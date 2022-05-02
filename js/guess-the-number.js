f258dc2337e276ea0d6b889a82112b189ebcc840
Tim Crisp <timothycrisp@gmail.com>
2022-05-02 17:14:25 -0500

    write init function

--- js/guess-the-number.js
+++ js/guess-the-number.js
@@ -5,7 +5,7 @@
 /*-------------------------------- Variables --------------------------------*/
 
 let secretNum, guessList, isWinner
-
+// 
 
 /*------------------------ Cached Element References ------------------------*/
 
@@ -19,7 +19,20 @@ const prevGuessMsg = document.querySelector("#prev-guesses-msg")
 
 /*----------------------------- Event Listeners -----------------------------*/
 
+resetBtn.addEventListener("reset", init)
 
 
 /*-------------------------------- Functions --------------------------------*/
+// our init function should take all of our state and reset it to our initial values
 
+init();
+
+function init() {
+  guessesEl.textContent = ""
+  messagesEl.textContent = "Please enter a guess between 1 and 100"
+  resetBtn.setAttribute("hidden", true)
+  prevGuessMsg.textContent = ""
+  guessList = []
+  isWinner = false
+  secretNum = Math.floor(Math.random() * 100 + 1)
+}
