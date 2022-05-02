84a5fbbda68ae1f6f33ec30b3c9910a5f5d022b1
Tim Crisp <timothycrisp@gmail.com>
2022-05-02 16:23:26 -0500

    add secret number method to game object

--- js/methods.js
+++ js/methods.js
@@ -2,8 +2,18 @@ const game = {
   title: 'Guess the Number!',
   biggestNum: 100,
   smallestNum: 1,
-  numGuesses: 0
+  numGuesses: 0,
+  secretNum: null,
+  play: function() {
+    this.secretNum = Math.floor(Math.random() * 
+      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+  }
 }
+
+// this refers to the parent object
+// this is the object that the function is a property of
+// this refers to the stuff that is on the left of the .
+// this refers to the object that it is in
 
 Object.keys(game).forEach(function(key) {
   console.log(key)
@@ -20,7 +30,4 @@ for (const [key, value] of Object.entries(game)) {
 
 console.log(Object.entries(game))
 
-game.play = function() {
-  this.secretNum = Math.floor(Math.random() * 
-    (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-}
+console.log(game)
