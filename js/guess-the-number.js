278d73c10200262cabe68d09c874334a15aecf2d
Tim Crisp <timothycrisp@gmail.com>
2022-05-02 16:52:28 -0500

    add cached element references

--- js/guess-the-number.js
+++ js/guess-the-number.js
@@ -4,10 +4,17 @@
 
 /*-------------------------------- Variables --------------------------------*/
 
+let secretNum, guessList, isWinner
 
 
 /*------------------------ Cached Element References ------------------------*/
 
+const form = document.querySelector("form")
+const guessInput = document.getElementById("#guess-input")
+const guessesEl = document.getElementById("#prev-guesses")
+const messagesEl = document.getElementById("#message")
+const resetBtn = document.querySelector("#reset-button")
+const prevGuessMsg = document.querySelector("#prev-guesses-msg")
 
 
 /*----------------------------- Event Listeners -----------------------------*/
